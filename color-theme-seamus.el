7e50eb636eb966d8cd96198b23e1781448f469a7
Bart Lantz <underhilllabs@gmail.com>
2011-05-18 20:15:35 -0600

    updated color-theme-seamus added colors for orgmode

--- color-theme-seamus.el
+++ color-theme-seamus.el
@@ -11,7 +11,7 @@
 ;; 
 
 (defun color-theme-seamus ()
-  "`color-theme-gnome2' via 'color-theme-robin-hood' with with wheat on charcoal.
+  "`color-theme-gnome2' via `color-theme-robin-hood'  with wheat on charcoal.
   I added colors for magit.  Maintained by Bart Lantz (underhilllabs@gmail.com)"
   (interactive)
   (color-theme-gnome2)
@@ -35,18 +35,28 @@
        (isearch ((t (:foreground "pink" :background "red"))))
        (isearch-lazy-highlight-face ((t (:foreground "red"))))
        ;; magit stuff!!
-       (magit-file-header ((t (:foreground "violet"))))
-       (magit-hunk-header ((t (:foreground "MediumSlateBlue"))))
+       (diff-file-header ((t (:foreground "violet"))))
+       (diff-hunk-header ((t (:foreground "MediumSlateBlue"))))
        (magit-header ((t (:foreground "cyan"))))
        (magit-tag-label ((t (:background "blue" :foreground "orange"))))
        (magit-diff-add ((t (:foreground "green3"))))
        (magit-diff-del ((t (:foreground "red3"))))
        (magit-item-highlight ((t (:background "#000012"))))
+       (magic-log-sha1 ((t (:foreground "red2"))))
        (menu ((t (:background "#304020" :foreground "navajo white"))))
        (minibuffer-prompt ((t (:foreground "pale green"))))
        (modeline ((t (:background "#464646" :foreground "wheat" :box (:line-width 1 :style released-button)))))
        (mode-line-inactive ((t (:background "#464646" :foreground "khaki" :box (:line-width 1 :style released-button)))))
        (modeline-buffer-id ((t (:background "#464646" :foreground "maroon")))) 
+       ;; orgmode
+       ;;(org-level-1 ((t ,ol1)))
+       ;;(org-level-2 ((t :)))
+       (org-level-3 ((t (:foreground "pale green"))))
+       (org-level-4 ((t (:foreground "#a9deff"))))
+       (org-level-5 ((t (:foreground "#ababcd"))))
+       ;;(org-level-6 ((t ,ol6)))
+       ;;(org-level-7 ((t ,ol7)))
+       ;;(org-level-8 ((t ,ol8)))
        (semantic-dirty-token-face ((t (:background "grey22"))))
        (tool-bar ((t (:background "#252525" :foreground "wheat" :box (:line-width 1 :style released-button)))))
        (tooltip ((t (:background "lemon chiffon" :foreground "black"))))))))
